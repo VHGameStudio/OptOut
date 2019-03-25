cba92500e71ceb9496794864081d45c8a78d76f9
victor <priorityvctr@gmail.com>
2019-03-25 14:56:52 +1000

    initial dev commit

---
+++ core/src/com/optout/GameRun.java
@@ -0,0 +1,16 @@
+package com.optout;
+
+import com.badlogic.gdx.Game;
+
+
+public class GameRun extends Game {
+	@Override
+	public void create() {
+
+	}
+
+	@Override
+	public void dispose() {
+		screen.dispose();
+	}
+}
